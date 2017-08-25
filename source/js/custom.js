5d9eb587fd20a10858efed4e69803a484a76c9d2
longht1989 <thanhlong.vn09@gmail.com>
2017-08-25 15:42:24 +0700

    HTML-BaoChinhPhu-EN

---
+++ source/js/custom.js
@@ -0,0 +1,32 @@
+/* ====================================
+   Onload functions
+   ==================================== */
+
+$(function() {
+    // highlight slider
+    $('.zone--highlight .slider').bxSlider({
+        nextText: '',
+        prevText: '',
+        nextSelector: '#highlight-slider-next',
+        prevSelector: '#highlight-slider-prev',
+        pagerCustom: '#highlight-pager'
+    });
+    // featured slider
+    $('.zone--slider .slider').bxSlider({
+        pager: false,
+        nextText: '',
+        prevText: '',
+        nextSelector: '#featured-slider-next',
+        prevSelector: '#featured-slider-prev'
+    });
+    // galleria article
+    $(function() {
+        // Load the classic theme
+        Galleria.loadTheme('../Jscripts/vendor/galleria/themes/classic/galleria.classic.min.js');
+
+        // Initialize Galleria
+        Galleria.run('#galleria', {
+            height: 400
+        });
+    });
+});
